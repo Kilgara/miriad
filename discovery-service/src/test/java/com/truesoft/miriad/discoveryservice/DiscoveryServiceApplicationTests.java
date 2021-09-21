7881406933bb2b59faa48735b3f47475c9d4ae37
rosviner <v-rosviner@vrbo.com>
2021-09-21 18:35:10 +0300

    add discovery service

---
+++ discovery-service/src/test/java/com/truesoft/miriad/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truesoft.miriad.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
