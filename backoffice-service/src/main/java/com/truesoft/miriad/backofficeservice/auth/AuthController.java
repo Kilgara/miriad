cbd48e122c2543f00fd475fe0ef621f4ad5147fb
rosviner <v-rosviner@vrbo.com>
2021-09-23 17:23:07 +0300

    add backoffice service

---
+++ backoffice-service/src/main/java/com/truesoft/miriad/backofficeservice/auth/AuthController.java
@@ -0,0 +1,15 @@
+package com.truesoft.miriad.backofficeservice.auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public void login() {
+
+    }
+}
---
+++ backoffice-service/src/test/java/com/truesoft/miriad/backofficeservice/BackofficeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truesoft.miriad.backofficeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
